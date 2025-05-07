8467a485e5a592ce62fa78bfb37588f8317dcea4
NdamageEsther <itgroupbngoma@gmail.com>
2025-05-07 18:14:24 +0200

    Add files via upload

---
+++ esther front cat/script.js
@@ -0,0 +1,11 @@
+(() => {
+    'use strict';
+    const form = document.getElementById('wasteForm');
+    form.addEventListener('submit', (e) => {
+        if (!form.checkValidity()) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        form.classList.add('was-validated');
+    });
+})();
